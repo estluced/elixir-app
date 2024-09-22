cc55681fb00ec6567ded63c7dc11c914fcccb1e0
Serhii Mykolenko <estluced@gmail.com>
2024-09-22 19:02:17 +0300

    chore: version bump

--- forge.config.ts
+++ forge.config.ts
@@ -9,62 +9,63 @@ import { rendererConfig } from './webpack.renderer.config'
 import getConfig from './src/utils/getConfig'
 
 const config: ForgeConfig = {
-  packagerConfig: {
-    asar: true,
-    icon: resolve(process.cwd(), join('public', 'icon', 'icon')),
-    extraResource: [resolve(process.cwd(), join('public', 'icon', 'icon.ico'))],
-  },
-  rebuildConfig: {},
-  makers: [
-    new MakerSquirrel({
-      iconUrl: resolve(process.cwd(), join('public', 'icon', 'icon.ico')),
-      setupIcon: resolve(process.cwd(), join('public', 'icon', 'icon.ico')),
-    }),
-  ],
-  plugins: [
-    new AutoUnpackNativesPlugin({}),
-    new WebpackPlugin({
-      mainConfig,
-      devContentSecurityPolicy:
-        "default-src * self blob: data: gap: file:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap: file:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap: file:; connect-src self * 'unsafe-inline' blob: data: gap: file:; frame-src * self blob: data: gap: file:;",
-      renderer: {
-        config: rendererConfig,
-        entryPoints: [
-          {
-            html: './src/preloader/index.html',
-            js: './src/preloader/index.ts',
-            name: 'preloader_window',
-          },
-          {
-            html: './src/console/index.html',
-            js: './src/console/index.ts',
-            name: 'console_window',
-            preload: {
-              js: './src/main/preload.ts',
-            },
-          },
-          {
-            html: './src/renderer/index.html',
-            js: './src/renderer/index.tsx',
-            name: 'main_window',
-            preload: {
-              js: './src/main/preload.ts',
-            },
-          },
-        ],
-      },
-    }),
-  ],
-  publishers: [
-    {
-      name: '@electron-forge/publisher-electron-release-server',
-      config: {
-        baseUrl: process.env.RELEASE_SERVER_URL,
-        username: process.env.RELEASE_SERVER_USER,
-        password: process.env.RELEASE_SERVER_PASSWORD,
-      },
-    },
-  ],
+	packagerConfig: {
+		asar: true,
+		icon: resolve(process.cwd(), join('public', 'icon', 'icon')),
+		extraResource: [resolve(process.cwd(), join('public', 'icon', 'icon.ico'))],
+	},
+	rebuildConfig: {},
+	makers: [
+		new MakerSquirrel({
+			iconUrl: resolve(process.cwd(), join('public', 'icon', 'icon.ico')),
+			setupIcon: resolve(process.cwd(), join('public', 'icon', 'icon.ico')),
+		}),
+	],
+	plugins: [
+		new AutoUnpackNativesPlugin({}),
+		new WebpackPlugin({
+			mainConfig,
+			devContentSecurityPolicy:
+				"default-src * self blob: data: gap: file:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap: file:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap: file:; connect-src self * 'unsafe-inline' blob: data: gap: file:; frame-src * self blob: data: gap: file:;",
+			renderer: {
+				config: rendererConfig,
+				entryPoints: [
+					{
+						html: './src/preloader/index.html',
+						js: './src/preloader/index.ts',
+						name: 'preloader_window',
+					},
+					{
+						html: './src/console/index.html',
+						js: './src/console/index.ts',
+						name: 'console_window',
+						preload: {
+							js: './src/main/preload.ts',
+						},
+					},
+					{
+						html: './src/renderer/index.html',
+						js: './src/renderer/index.tsx',
+						name: 'main_window',
+						preload: {
+							js: './src/main/preload.ts',
+						},
+					},
+				],
+			},
+		}),
+	],
+	publishers: [
+		{
+			name: '@electron-forge/publisher-electron-release-server',
+			config: {
+				baseUrl: 'http://213.199.38.9:83',
+				username: 'admin',
+				password:
+					'electron-updater-publish-password-SJB3454895GBFFHDBKJV394Q3BFJSKA',
+			},
+		},
+	],
 }
 
 export default config
